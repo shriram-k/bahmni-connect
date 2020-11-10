559603013121f45d0822792528b9dc2c08af5337
Aakash Maurya <aakash.maurya@thoughtworks.com>
2020-11-11 01:48:32 +0530

    RBC-32 | Aakash, Rakesh | Separate lists for filters for province and district.

--- ui/app/syncdatarules/controllers/SyncDataRulesController.js
+++ ui/app/syncdatarules/controllers/SyncDataRulesController.js
@@ -1,25 +1,39 @@
 'use strict';
 
 angular.module('syncdatarules')
-  .controller('SyncDataRulesController', ['$scope', '$rootScope', '$http', '$q', 'offlineDbService',
-      function ($scope, $rootScope, $http, $q, offlineDbService) {
-          $scope.addressList = [];
-          var addAddress = function (address) {
-              $scope.addressList.push(address);
+  .controller('SyncDataRulesController', ['$scope', 'offlineDbService',
+      function ($scope, offlineDbService) {
+          var LEVEL_PROVINCE;
+          var LEVEL_DISTRICT;
+
+          $scope.provinceAddressList = [];
+          $scope.districtAddressList = [];
+
+          var addProviceAddress = function (address) {
+              $scope.provinceAddressList.push(address);
+          };
+
+          var addDistrictAddress = function (address) {
+              $scope.districtAddressList.push(address);
           };
 
           $scope.populateList = function () {
-              offlineDbService.getAddressesHeirarchyLevels().then(// 3,4
+              offlineDbService.getAddressesHeirarchyLevels().then(
             function (levels) {
                 var levelIds = levels.map(id => id.addressHierarchyLevelId);
-                console.log("levels: " + levelIds);
+                LEVEL_PROVINCE = levelIds[0];
+                LEVEL_DISTRICT = levelIds[1];
 
                 levelIds.forEach(function (id) {
                     offlineDbService.getAllAddressesByLevelId(id).then(
-                function (add) {
-                    add.map(address => addAddress(address));
-                }
-              );
+                    function (add) {
+                        if (id === LEVEL_PROVINCE) {
+                            add.map(address => addProviceAddress(address));
+                        } else if (id === LEVEL_DISTRICT) {
+                            add.map(address => addDistrictAddress(address));
+                        }
+                    }
+                  );
                 });
             }
         );
