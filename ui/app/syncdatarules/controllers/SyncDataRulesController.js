3f3044af7e744acbdfa030d2b7f1400094d04a93
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2020-11-20 14:59:26 +0530

    RBC-18 | Rakesh,Gopi | Added Logic To Show Next AddressLevels Based on previous Level Selction

--- ui/app/syncdatarules/controllers/SyncDataRulesController.js
+++ ui/app/syncdatarules/controllers/SyncDataRulesController.js
@@ -2,35 +2,21 @@
 
 angular.module("syncdatarules").controller("SyncDataRulesController", [
   "$scope",
-  "offlineDbService","offlineService",
-  function ($scope, offlineDbService,offlineService) {
+  "offlineDbService", "offlineService",
+  function ($scope, offlineDbService, offlineService) {
 
-    $('.selected-items-box').unbind('click').bind('click', function(e) {
-
-      if(e.currentTarget.id == "Province-select") {
-        $('.Province-list').slideToggle('fast');
-      }
-      else if(e.currentTarget.id == "district-select") {
-        $('.district-list').slideToggle('fast');
-      }
-      else if(e.currentTarget.id == "facility-select") {
-        $('.facility-list').slideToggle('fast');
-      }
-
-    });
-
-    $(document).mouseup(function(e) 
-    {
-      var container = new Array();
-      container.push($('.list'));
-      $.each(container, function(key, value) {
-        if (!$(value).is(e.target) // if the target of the click isn't the container...
-            && $(value).has(e.target).length === 0) // ... nor a descendant of the container
-        {
-            $(value).hide();
-        }
-      });
-    });
+    // $(document).mouseup(function(e) 
+    // {
+    //   var container = new Array();
+    //   container.push($('.list'));
+    //   $.each(container, function(key, value) {
+    //     if (!$(value).is(e.target) // if the target of the click isn't the container...
+    //         && $(value).has(e.target).length === 0) // ... nor a descendant of the container
+    //     {
+    //         $(value).hide();
+    //     }
+    //   });
+    // });
 
     var LEVEL_PROVINCE;
     var LEVEL_DISTRICT;
@@ -45,7 +31,7 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
       filteredDistrictList: [],
       filteredFacilityList: [],
       isSelectVisible: false,
-      validationError: "** Please Select Province **", 
+      validationError: "** Please Select Province **",
       showValidationError: false,
       isDataAvailable: false
     };
@@ -64,28 +50,52 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
 
     $scope.filterDistrict = function () {
       resetSecondaryFilters();
-      var selectedProvincesParentIds =  $scope.state.provinceAddressList.filter(province => province.selected).map(province => province.id);
+      var selectedProvincesParentIds = $scope.state.provinceAddressList.filter(province => province.selected).map(province => province.id);
       $scope.state.filteredDistrictList = districtAddressList.filter(dist => selectedProvincesParentIds.includes(dist.parentId));
     };
 
-    $scope.filterFacility = function() {
-      var selectedDistrictParentIds =  districtAddressList.filter(district => district.selected).map(district => district.id);
+    $scope.filterFacility = function () {
+      var selectedDistrictParentIds = districtAddressList.filter(district => district.selected).map(district => district.id);
       $scope.state.filteredFacilityList = facilityAddressList.filter(fac => selectedDistrictParentIds.includes(fac.parentId));
     };
 
-    $scope.selectedProvinceNames = function() {
+    $scope.selectedProvinceNames = function () {
       return $scope.state.provinceAddressList.filter(
         province => province.selected
       ).map(prov => prov.name)
     };
 
-    $scope.selectedDistrictNames = function() {
+    $scope.selecteLevelNames = function (level) {
+        if ($scope.addresses.hasOwnProperty(level)) {
+          return $scope.addresses[level].filter(
+            address => address.selected
+          ).map(prov => prov.name)
+        }
+    };
+
+    $scope.filterLevels = function(level){//Province_0
+      let targetToShow = parseInt(level.split('_')[1]) + 1;
+      targetToShow += '-block';
+      let temp = '#' + targetToShow;
+      $scope.idsToShow.push(temp);
+      var selectedParentIds = $scope.addresses[level].filter(province => province.selected).map(province => province.id);
+      var indexToMatch = parseInt(level.split('_')[1]) + 1;
+      for (let key in $scope.addresses) {
+            if(key.includes(indexToMatch))
+            {
+              $scope.addresses[key] = $scope.addresses[key].filter(levelToFilter => selectedParentIds.includes(levelToFilter.parentId));
+            }
+      }
+      //$scope.state.filteredDistrictList = districtAddressList.filter(dist => selectedProvincesParentIds.includes(dist.parentId));
+    };
+
+    $scope.selectedDistrictNames = function () {
       return $scope.state.filteredDistrictList.filter(
         dist => dist.selected
       ).map(prov => prov.name)
     };
 
-    $scope.selectedFacilityNames = function() {
+    $scope.selectedFacilityNames = function () {
       return $scope.state.filteredFacilityList.filter(
         fac => fac.selected
       ).map(fac => fac.name)
@@ -102,61 +112,74 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
       $scope.state.provinceAddressList.map(province => province.selected = false)
     };
 
-		$scope.loadState = function(){
+    $scope.loadState = function () {
       $scope.state;
       $scope.addresses;
-      for(let key in $scope.addresses)
-      {
-        let idToHide = '#'+ key.split("_")[1] + '-block';
-        if(key.split("_")[1] === '0'){
+      for (let key in $scope.addresses) {
+        let idToHide = '#' + key.split("_")[1] + '-block';
+        if (key.split("_")[1] === '0') {
           $scope.idsToShow.push(idToHide);
         }
       }
+
+      $('.selected-items-box').unbind('click').bind('click', function (e) {
+
+        if (e.currentTarget.id == "Province-select") {
+          $('.Province-list').slideToggle('fast');
+        }
+        else if (e.currentTarget.id == "District-select") {
+          $('.District-list').slideToggle('fast');
+        }
+        else if (e.currentTarget.id == "Facility-select") {
+          $('.Facility-list').slideToggle('fast');
+        }
+
+      });
     };
 
-    $scope.handleDropDowns = function(selectedId) {
+    $scope.handleDropDowns = function (selectedId) {
       console.log(selectedId);
       let targetToShow = parseInt(selectedId) + 1;
       targetToShow += '-block';
-      let temp ='#'+targetToShow;
+      let temp = '#' + targetToShow;
       $scope.idsToShow.push(temp);
       $(temp).show();
     };
 
-    $scope.display = function(idOfBlock){
+    $scope.display = function (idOfBlock) {
       let temp = '#' + idOfBlock + '-block';
-      if($scope.idsToShow.indexOf(temp) == -1)
+      if ($scope.idsToShow.indexOf(temp) == -1)
         return false;
-        else 
+      else
         return true;
     }
 
-    $scope.sync = function(){
-      
-      if($scope.selectedProvinceNames().length === 0){
+    $scope.sync = function () {
+
+      if ($scope.selectedProvinceNames().length === 0) {
         $scope.state.showValidationError = true;
-      }else{
+      } else {
         var config = { "strategy": $scope.state.sync_stratergy, "Province": $scope.selectedProvinceNames(), "District": $scope.selectedDistrictNames(), "Facility": $scope.selectedFacilityNames() };
-        var filters = config.Province + (config.District.length !=0 ? ("-" +  config.District) : "") + (config.Facility.length !=0 ? ("-" + config.Facility) : "");
-        console.log('Filters' , filters);
+        var filters = config.Province + (config.District.length != 0 ? ("-" + config.District) : "") + (config.Facility.length != 0 ? ("-" + config.Facility) : "");
+        console.log('Filters', filters);
         $scope.state.showValidationError = false;
         //Override Marker.filters
         //if($scope.state.sync_stratergy === "selective"){
-          let categories = offlineService.getItem("eventLogCategories");
+        let categories = offlineService.getItem("eventLogCategories");
         _.forEach(categories, function (category) {
-          if(category === "patient" || category === "encounter"){
-           offlineDbService.getMarker(category).then(function(marker){
-            let tempMarkers = [];
-              _.forEach(marker.filters, function(markerEntry){
+          if (category === "patient" || category === "encounter") {
+            offlineDbService.getMarker(category).then(function (marker) {
+              let tempMarkers = [];
+              _.forEach(marker.filters, function (markerEntry) {
                 let filter = markerEntry.split("-")[0];
                 filter = filter + "-" + filters;
                 tempMarkers.push(filter);
               });
               offlineDbService.insertMarker(marker.markerName, marker.lastReadEventUuid, tempMarkers);
-          });
-        }
-      });
-       // logic to go to offlineSync service sync()
+            });
+          }
+        });
+        // logic to go to offlineSync service sync()
         //}
       }
     };
@@ -164,6 +187,7 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
 
 
     $scope.addresses = {}
+    $scope.addressesToFilter = $scope.addresses;
     $scope.idsToShow = [];
 
     $scope.populateList = function () {
@@ -172,14 +196,14 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
         levels.forEach(function (level, index) {
           offlineDbService.getAllAddressesByLevelId(level.addressHierarchyLevelId).then(function (address) {
             //$scope.addresses[`level_${index}`] = address;  
-            $scope.addresses[`${level.name}_${index}`] = address; 
-            $('#loadData').click(); 
+            $scope.addresses[`${level.name}_${index}`] = address;
+            $('#loadData').click();
           });
         });
-        
+
       });
-		};
-  
+    };
+
     //populateList();
   },
 ]);
