325aaa3f503e7d4672651f4e8a563a08633c690b
shriram-k <shriram.kraja@thoughtworks.com>
2020-11-24 13:28:58 +0530

    RBC-33 | Shriram, Aakash | Mader visual changes to the sync button, disabled button when sync in progress.
    Co-Authored-By: Aakash Maurya <aakash.maurya@thoughtworks.com>

--- ui/app/syncdatarules/controllers/SyncDataRulesController.js
+++ ui/app/syncdatarules/controllers/SyncDataRulesController.js
@@ -2,32 +2,40 @@
 
 angular.module("syncdatarules").controller("SyncDataRulesController", [
   "$scope",
-  "offlineDbService","offlineService",'schedulerService',
-  function ($scope, offlineDbService,offlineService,schedulerService) {
+  "offlineDbService", "offlineService", 'schedulerService', 'eventQueue','spinner', "$q",
+  function ($scope, offlineDbService, offlineService, schedulerService, eventQueue,spinner, $q) {
 
-    $('.selected-items-box').unbind('click').bind('click', function(e) {
+    $('.selected-items-box').unbind('click').bind('click', function (e) {
 
-      if(e.currentTarget.id == "province-select") {
+      if (e.currentTarget.id == "province-select") {
         $('.province-list').slideToggle('fast');
       }
-      else if(e.currentTarget.id == "district-select") {
+      else if (e.currentTarget.id == "district-select") {
         $('.district-list').slideToggle('fast');
       }
-      else if(e.currentTarget.id == "facility-select") {
+      else if (e.currentTarget.id == "facility-select") {
         $('.facility-list').slideToggle('fast');
       }
 
     });
 
-    $(document).mouseup(function(e)
-    {
+    $scope.isOfflineApp = offlineService.isOfflineApp();
+
+    var init = function () {
+      console.log("in sync data rules init: ", $scope.isOfflineApp);
+      if ($scope.isOfflineApp) {
+          setWatchersForErrorStatus();
+      }
+  };
+
+    $(document).mouseup(function (e) {
       var container = new Array();
       container.push($('.list'));
-      $.each(container, function(key, value) {
+      $.each(container, function (key, value) {
         if (!$(value).is(e.target) // if the target of the click isn't the container...
-            && $(value).has(e.target).length === 0) // ... nor a descendant of the container
+          && $(value).has(e.target).length === 0) // ... nor a descendant of the container
         {
-            $(value).hide();
+          $(value).hide();
         }
       });
     });
@@ -63,28 +71,28 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
 
     $scope.filterDistrict = function () {
       resetSecondaryFilters();
-      var selectedProvincesParentIds =  $scope.state.provinceAddressList.filter(province => province.selected).map(province => province.id);
+      var selectedProvincesParentIds = $scope.state.provinceAddressList.filter(province => province.selected).map(province => province.id);
       $scope.state.filteredDistrictList = districtAddressList.filter(dist => selectedProvincesParentIds.includes(dist.parentId));
     };
 
-    $scope.filterFacility = function() {
-      var selectedDistrictParentIds =  districtAddressList.filter(district => district.selected).map(district => district.id);
+    $scope.filterFacility = function () {
+      var selectedDistrictParentIds = districtAddressList.filter(district => district.selected).map(district => district.id);
       $scope.state.filteredFacilityList = facilityAddressList.filter(fac => selectedDistrictParentIds.includes(fac.parentId));
     };
 
-    $scope.selectedProvinceNames = function() {
+    $scope.selectedProvinceNames = function () {
       return $scope.state.provinceAddressList.filter(
         province => province.selected
       ).map(prov => prov.name)
     };
 
-    $scope.selectedDistrictNames = function() {
+    $scope.selectedDistrictNames = function () {
       return $scope.state.filteredDistrictList.filter(
         dist => dist.selected
       ).map(prov => prov.name)
     };
 
-    $scope.selectedFacilityNames = function() {
+    $scope.selectedFacilityNames = function () {
       return $scope.state.filteredFacilityList.filter(
         fac => fac.selected
       ).map(fac => fac.name)
@@ -101,53 +109,123 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
       $scope.state.provinceAddressList.map(province => province.selected = false)
     };
 
-		$scope.showSelect = function(val){
+    $scope.showSelect = function (val) {
       $scope.state.isSelectVisible = val == 'Y';
-      if( val == 'N') $scope.state.showValidationError = false;
+      if (val == 'N') $scope.state.showValidationError = false;
     };
 
-    $scope.sync = function(){
+    var cleanUpListenerSchedulerStage = $scope.$on("schedulerStage", function (event, stage, restartSync) {
+      $scope.isSyncing = (stage !== null);
+      if (restartSync) {
+          schedulerService.stopSync();
+          schedulerService.sync();
+      }
+  });
 
-      if($scope.state.sync_stratergy == "selective" && $scope.selectedProvinceNames().length === 0){
+  $scope.$on("$destroy", cleanUpListenerSchedulerStage);
+
+    $scope.getStatusStyleCode = function () {
+      return $scope.syncStatusMessage && ($scope.syncStatusMessage.match(/.*Success.*/i) ? 'success' : $scope.syncStatusMessage.match(/.*Pending.*/i) ? 'pending' : $scope.syncStatusMessage.match(/.*Failed.*/i) ? 'fail' : 'inProgress');
+    };
+
+    var getLastSyncTime = function () {
+      var date = offlineService.getItem('lastSyncTime');
+      var localeDate = Bahmni.Common.Util.DateUtil.parseServerDateToDate(date);
+      $scope.lastSyncTime = Bahmni.Common.Util.DateUtil.getDateTimeInSpecifiedFormat(localeDate, "dddd, MMMM Do YYYY, HH:mm:ss");
+    };
+
+    var getErrorCount = function () {
+      return eventQueue.getErrorCount().then(function (errorCount) {
+        return errorCount;
+      });
+    };
+
+    var getEventCount = function () {
+      return eventQueue.getCount().then(function (eventCount) {
+        return eventCount;
+      });
+    };
+
+    var updateSyncStatusMessageBasedOnQueuesCount = function () {
+      getErrorCount().then(function (errorCount) {
+        if (errorCount) {
+          $scope.syncStatusMessage = Bahmni.Common.Constants.syncStatusMessages.syncFailed;
+        } else {
+          getEventCount().then(function (eventCount) {
+            $scope.syncStatusMessage = eventCount ? Bahmni.Common.Constants.syncStatusMessages.syncPending : updateLastSyncTimeOnSuccessfullSyncAnReturnSuccessMessage();
+          });
+        }
+      });
+    };
+
+    var updateLastSyncTimeOnSuccessfullSyncAnReturnSuccessMessage = function () {
+      if ($scope.isSyncing !== undefined) {
+        offlineService.setItem('lastSyncTime', new Date());
+        getLastSyncTime();
+      }
+      return Bahmni.Common.Constants.syncStatusMessages.syncSuccess;
+    };
+
+    var getSyncStatusInfo = function () {
+      getLastSyncTime();
+      $scope.isSyncing ? $scope.syncStatusMessage = "Sync in Progress..." : updateSyncStatusMessageBasedOnQueuesCount();
+    };
+    getSyncStatusInfo();
+
+    var setErrorStatusOnErrorsInSync = function () {
+      offlineDbService.getAllLogs().then(function (errors) {
+          $scope.errorsInSync = !!errors.length;
+      });
+  };
+
+    var setWatchersForErrorStatus = function () {
+      $scope.$watch('isSyncing', function () {
+          getSyncStatusInfo();
+          setErrorStatusOnErrorsInSync();
+      });
+  };
+
+    $scope.sync = function () {
+      if ($scope.state.sync_stratergy == "selective" && $scope.selectedProvinceNames().length === 0) {
         $scope.state.showValidationError = true;
-      }else{
+      } else {
         var config = { "strategy": $scope.state.sync_stratergy, "Province": $scope.selectedProvinceNames(), "District": $scope.selectedDistrictNames(), "Facility": $scope.selectedFacilityNames() };
-        var filters = config.Province + (config.District.length !=0 ? ("-" +  config.District) : "") + (config.Facility.length !=0 ? ("-" + config.Facility) : "");
-        console.log('Filters' , filters);
+        var filters = config.Province + (config.District.length != 0 ? ("-" + config.District) : "") + (config.Facility.length != 0 ? ("-" + config.Facility) : "");
+        console.log('Filters', filters);
         $scope.state.showValidationError = false;
         //Override Marker.filters
-        if($scope.state.sync_stratergy === "selective"){
+        if ($scope.state.sync_stratergy === "selective") {
           let categories = offlineService.getItem("eventLogCategories");
-        _.forEach(categories, function (category) {
-          if(category === "patient" || category === "encounter"){
-           offlineDbService.getMarker(category).then(function(marker){
-            let tempMarkers = [];
-              _.forEach(marker.filters, function(markerEntry){
-                let filter = markerEntry.split("-")[0];
-                filter = filter + "-" + filters;
-                tempMarkers.push(filter);
+          _.forEach(categories, function (category) {
+            if (category === "patient" || category === "encounter") {
+              offlineDbService.getMarker(category).then(function (marker) {
+                let tempMarkers = [];
+                _.forEach(marker.filters, function (markerEntry) {
+                  let filter = markerEntry.split("-")[0];
+                  filter = filter + "-" + filters;
+                  tempMarkers.push(filter);
+                });
+                offlineDbService.insertMarker(marker.markerName, marker.lastReadEventUuid, tempMarkers);
               });
-              offlineDbService.insertMarker(marker.markerName, marker.lastReadEventUuid, tempMarkers);
+            }
           });
+          // logic to go to offlineSync service sync()
         }
-      });
-       // logic to go to offlineSync service sync()
-        }
-        else{
+        else {
           // logic to go to offlineSync service sync()
           let categories = offlineService.getItem("eventLogCategories");
-        _.forEach(categories, function (category) {
-          if(category === "patient" || category === "encounter"){
-           offlineDbService.getMarker(category).then(function(marker){
-            let tempMarkers = [];
-              _.forEach(marker.filters, function(markerEntry){
-                let filter = markerEntry.split("-")[0];
-                tempMarkers.push(filter);
+          _.forEach(categories, function (category) {
+            if (category === "patient" || category === "encounter") {
+              offlineDbService.getMarker(category).then(function (marker) {
+                let tempMarkers = [];
+                _.forEach(marker.filters, function (markerEntry) {
+                  let filter = markerEntry.split("-")[0];
+                  tempMarkers.push(filter);
+                });
+                offlineDbService.insertMarker(marker.markerName, marker.lastReadEventUuid, tempMarkers);
               });
-              offlineDbService.insertMarker(marker.markerName, marker.lastReadEventUuid, tempMarkers);
+            }
           });
-        }
-      });
         }
 
         schedulerService.sync(Bahmni.Common.Constants.syncButtonConfiguration);
@@ -167,14 +245,15 @@ angular.module("syncdatarules").controller("SyncDataRulesController", [
               add.map((address) => addProviceAddress(address));
             } else if (id === LEVEL_DISTRICT) {
               add.map((address) => addDistrictAddress(address));
-					  } else if (id === LEVEL_FACILITY) {
+            } else if (id === LEVEL_FACILITY) {
               add.map((address) => addFacilityAddress(address));
-	    		  }
+            }
           });
         });
       });
-		};
+    };
 
     populateList();
+    return spinner.forPromise($q.all(init()));
   },
 ]);
