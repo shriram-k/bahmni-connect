cc4735d81f2f91f5e090946eba3e228ad2e02434
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2020-12-03 11:50:10 +0530

    RBC-45 | Gopi,Rakesh | Updated Testcases and Updated Logic to compare addressField

--- ui/app/common/offline/dbservices/dao/offlineAddressHierarchyDbService.js
+++ ui/app/common/offline/dbservices/dao/offlineAddressHierarchyDbService.js
@@ -57,14 +57,17 @@ angular.module('bahmni.common.offline')
             var level;
 
             for (var i in addressFields) {
-                if (addressFields[i] === params.addressField) {
-                    addressHierarchyField = addressFields[i];
-                    // if(params.strategy && params.strategy == 'SelectiveSync')
-                    // addressHierarchyField = addressFields[i];
-                    // else
-                    // addressHierarchyField = i;
+                if (params.strategy && params.strategy == 'SelectiveSync') {
+                    if (i === params.addressField) {
+                        addressHierarchyField = i;
+                    }
                 }
-            }
+                    else {
+                        if (addressFields[i] === params.addressField) {
+                            addressHierarchyField = i;
+                        }
+                    }
+                }
             return db.select()
                 .from(addressHierarchyLevelTable)
                 .where(addressHierarchyLevelTable.addressField.eq(addressHierarchyField)).exec()
